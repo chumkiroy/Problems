89266b179171425b82f0a756b8c69d3b24b0ff3a
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-02 11:40:21 -0800

    Shortest Distance From the Guard

---
+++ graph/Shortest_Distance_from_Guard.py
@@ -0,0 +1,10 @@
+'''
+Problem Statement: Shortest Distance From the Guard
+
+https://www.geeksforgeeks.org/find-shortest-distance-guard-bank/
+
+'''
+
+def find_shortest_distance_from_a_guard(grid)
+	# code
+	pass
