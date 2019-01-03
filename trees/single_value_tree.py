9bdddde766454d81fdc2183167aed9e49574095e
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-03 10:01:57 -0800

    Single Value Tree - h2

---
+++ trees/single_value_tree.py
@@ -0,0 +1,43 @@
+'''
+PROBLEM STATEMENT: Single Value Tree
+
+Given a binary tree, we need to count the number of unival subtrees (all nodes that have the same value). 
+e.g. Given the following tree, it has 6 unival subtrees.
+
+Custom Input Format:
+First line contains single integer denoting total no of nodes in the tree.
+Seconds line contains pre-order traversal of tree (values are space separated).
+
+For example:
+3
+1 # #
+
+Denotes tree like:
+    1
+  /   \
+ null null
+--------------------------------------
+
+7
+1 2 3 # # # #
+
+Denotes tree like:
+       1
+      /   \
+     2    null
+    /  \
+   3   null
+  /  \
+ null  null
+
+Use the option "Show Input/Output Code " just above the code editor, to see, how input is read, tree is built, the function that you are going to complete is called and output is printed.
+
+Note:
+-----
+https://crazycoderzz.wordpress.com/count-the-number-of-unival-subtrees-in-a-binary-tree/
+https://www.geeksforgeeks.org/find-count-of-singly-subtrees/
+
+'''
+
+def  findSingleValueTrees(node):
+	pass # CODE
