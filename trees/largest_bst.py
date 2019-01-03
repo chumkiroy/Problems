6a40e7e0f7d3f218d75ab4c9dd11293d92eff387
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-03 12:38:12 -0800

    Largest BST - h2

---
+++ trees/largest_bst.py
@@ -0,0 +1,48 @@
+'''
+PROBLEM STATEMENT: Largest BST
+
+Given a binary tree, find the largest Binary Search Tree (BST), where largest means BST with largest number of nodes in it. The largest BST must include all of its descendants.
+
+Custom Input Format:
+--------------------
+First line contains single integer denoting total no of nodes in the tree.
+
+Seconds line contains pre-order traversal of tree (values are space separated).
+
+For example:
+------------
+3
+1 # #
+
+Denotes tree like:
+    1
+  /   \
+ null null
+
+--------------------------------------
+
+7
+1 2 3 # # # #
+
+Denotes tree like:
+       1
+      /   \
+     2    null
+    /  \
+   3   null
+  /  \
+ null  null
+
+Use the option "Show Input/Output Code " just above the code editor, to see, how input is read, tree is built, the function that you are going to complete is called and output is printed.
+
+Notes:
+------
+Suggested time: 30 minutes.
+
+https://articles.leetcode.com/largest-binary-search-tree-bst-in_22/
+http://www.ideserve.co.in/learn/size-of-largest-bst-in-binary-tree
+
+'''
+
+def  findLargestBST(node):
+	pass
