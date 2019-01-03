6c1ce4dcbef6813d9fc4f31b12c1466df2950cc2
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-03 10:07:06 -0800

    PostOrder Traversal without recursion - h2

---
+++ trees/post_order_traversal_no_rec.py
@@ -0,0 +1,45 @@
+'''
+PROBLEM STATEMENT: PostOrder traversal
+
+Write a function to traverse a Binary tree PostOrder, without using recursion. As you traverse, please print contents of the nodes.
+
+Custom Input Format:
+---------------------
+First line contains single integer denoting total no of nodes in the tree.
+
+Seconds line contains pre-order traversal of tree (values are space separated).
+
+For example:
+------------
+3
+1 # #
+
+Denotes tree like:
+    1
+  /   \
+ null null
+--------------------------------------
+
+7
+1 2 3 # # # #
+
+Denotes tree like:
+       1
+      /   \
+     2    null
+    /  \
+   3   null
+  /  \
+ null  null
+
+Use the option "Show Input/Output Code " just above the code editor, to see, how input is read, tree is built, the function that you are going to complete is called and output is printed.
+
+Note:
+-----
+https://www.youtube.com/watch?v=hv-mJUs5mvU
+https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+'''
+
+def  postorderTraversal(root):
+	pass #CODE
