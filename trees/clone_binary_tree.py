c6cf72bc03d9954ea954b39b2e401e545e487ece
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-03 11:47:30 -0800

    Clone a binary tree

---
+++ trees/clone_binary_tree.py
@@ -0,0 +1,47 @@
+'''
+PROBLEM STATEMENT: CLONE A BINARY TREE
+
+Given a binary tree (represented by its root node, like usual), clone it. Return the root node of the cloned tree.
+
+Remember: Cloning or copying a tree is best done recursively. Notice how clean and succinct the code is. Some of you may be tempted to do it breadth-first. But that's more complicated to handle in implementation. 
+Custom Input Format:
+--------------------
+First line contains single integer denoting total no of nodes in the tree.
+Seconds line contains pre-order traversal of tree (values are space separated).
+
+For example:
+------------
+3
+1 # #
+
+Denotes tree like:
+    1
+  /   \
+ null null
+--------------------------------------
+
+7
+1 2 3 # # # #
+
+Denotes tree like:
+       1
+      /   \
+     2    null
+    /  \
+   3   null
+  /  \
+ null  null
+
+Use the option "Show Input/Output Code " just above the code editor, to see, how input is read, tree is built, the function that you are going to complete is called and output is printed.
+
+Notes:
+------
+Suggested time: 20 minutes.
+
+https://coding-interview-solutions.hackingnote.com/problems/clone-binary-tree.html
+http://crackprogramming.blogspot.com/2012/11/make-copy-of-binary-tree-given-pointer.html
+
+'''
+
+def cloneTree(node):
+	pass #CODE
