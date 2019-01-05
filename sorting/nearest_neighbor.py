331950e0254788161ce02117e270d6285935e57d
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-04 20:39:28 -0800

    Nearest neighbor - h2
    Given a point P, and other N points in two dimensional space, find K points out of the N points which are nearest to P.

---
+++ sorting/nearest_neighbor.py
@@ -0,0 +1,16 @@
+'''
+Problem Statement: Nearest neighbor
+
+Given a point P, and other N points in two dimensional space, find K points out of the N points which are nearest to P.
+
+* Distance between two points is measured by standard euclidean method.
+
+(Hint: This problem can either be done with QuickSort partitioning, or can be done with Heaps. Which one would you use? Why? Why not try both in your IDE and see the runtime for large inputs?)
+
+Note:
+https://www.geeksforgeeks.org/k-nearest-neighbours/
+http://stackoverflow.com/questions/20398799/find-k-nearest-points-to-point-p-in-2-dimensional-plane
+'''
+
+def find_nearest_neighbours(px, py, n_points, k):
+	pass #code
