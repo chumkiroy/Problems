12b494672df4bc99c6f030c875e6163854f5a117
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-04 21:13:30 -0800

    Sort all characters in a string

---
+++ sorting/sort_all_character_in_string.py
@@ -0,0 +1,20 @@
+'''
+Problem Statement: Sort all characters in a string
+
+Sort an array of characters (ASCII only, not UTF8).
+
+Input: A string of characters, like a full English sentence, delimited by a newline or NULL. Duplicates are okay.
+
+Output: A string of characters, in sorted order of their ASCII values. You can overwrite the existing array.
+
+Solution Complexity: Aim for linear time and constant additional space.
+
+(What to understand from this problem: ASCII is great, because it's limited to 256. Remember that for any input that is bound to a range)
+
+Note:
+https://www.geeksforgeeks.org/sort-string-characters/
+
+'''
+
+def sortCharacters(inString):
+	pass #Code
