17e0e78d615c450fe4bfc46e64fb1bd1b62d9ac3
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-04 20:42:22 -0800

    Implement Merge Sort:
    Please convert the standard merge-sort algorithm into code.

---
+++ sorting/implement_merge_sort.py
@@ -0,0 +1,21 @@
+'''
+Problem Statement:  Implement Merge Sort
+
+Please convert the standard merge-sort algorithm into code.
+
+Input: Integers in an array, duplicates possible
+
+Output: Same integers in ascending order, in a new array. Preserve the input array.
+
+(Goal of this homework problem: The concept of partitioning, concept of merging partitions, the clarity that merge-sort needs extra space and why worst-case is better than QuickSort viz. O(NLogN))
+
+Note:
+https://www.geeksforgeeks.org/merge-sort/
+http://www.codenlearn.com/2011/10/simple-merge-sort.html
+
+Question:
+In the solution above, what is the Complexity of memory requirement? Can we do better than that?
+
+'''
+def MergeSort(intArr):
+	pass #Code
