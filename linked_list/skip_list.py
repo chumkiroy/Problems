c0a8a2439d86de918eab11d08e51522c80d04409
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-08 01:32:15 -0800

    Skip Lists!

---
+++ linked_list/skip_list.py
@@ -0,0 +1,12 @@
+'''
+Skip Lists!
+
+Implement a simple Skip List. (Certain startups are enamored with Skip Lists in their interviews)
+
+Note:
+
+https://www.geeksforgeeks.org/skip-list/
+http://igoro.com/archive/skip-lists-are-fascinating/
+Bonus points: Read this discussion on StackOverflow, that compares and contrasts skip lists with BSTs: http://stackoverflow.com/questions/256511/skip-list-vs-binary-tree
+
+'''
