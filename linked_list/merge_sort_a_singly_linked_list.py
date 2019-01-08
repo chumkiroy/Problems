9edeae9433a2c6d71331b8bb3d3028ec6173c663
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-08 01:27:00 -0800

    Sort a singly linked list using merge sort. Sort ascending.

---
+++ linked_list/merge_sort_a_singly_linked_list.py
@@ -0,0 +1,19 @@
+'''
+Sort a singly linked list using merge sort. Sort ascending.
+
+(Trivia question: Merge Sort, in general is a good method of sorting anything, because its worst case performance is better than most other sorting methods. For Linked Lists however, Merge Sort is highly preferred, because it has a distinct advantage. What is that advantage?)
+
+Note:
+https://leetcode.com/problems/merge-two-sorted-lists/description/
+https://docs.google.com/document/d/1UBFl633FsCZLq2Vejihj2g80Vdl9BNF6-N1D4J-clsI/edit?usp=sharing
+
+'''
+
+#For your reference:
+#LinkedListNode {
+#    int val
+#    LinkedListNode next
+#}
+
+def  mergeSortList(pList):
+	pass
