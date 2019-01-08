9169aa701165e10f0139b4834c256249b2b37f8c
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-08 01:29:48 -0800

    Valid mathcing params:
    Write a function that checks if the given input string has matching opening and closing parentheses. Valid parantheses are: ‘(‘, ‘)’, ‘{‘, ‘}’, ‘[‘, ‘]’

---
+++ linked_list/valid_matching_param.py
@@ -0,0 +1,22 @@
+'''
+Valid Matching params:
+
+Write a function that checks if the given input string has matching opening and closing parentheses. Valid parantheses are: ‘(‘, ‘)’, ‘{‘, ‘}’, ‘[‘, ‘]’
+
+Test cases:
+
+Input: “( ( 1 + 2 ) * 3 )”, Output: True
+Input: “( { 1 + 2 ) * 3 )”, Output: False
+Input: “( ( (1 + 2) * 3 ))”, Output: True
+Input: “{([])}”, Output: True
+Input: “} ( 1 * 2) + 3 * ( 5 - 6)”: False
+
+
+Note:
+https://leetcode.com/problems/valid-parenthesis-string/description/
+https://docs.google.com/document/d/1UBFl633FsCZLq2Vejihj2g80Vdl9BNF6-N1D4J-clsI/edit?usp=sharing
+
+'''
+
+def hasMatchingParantheses(strExpression):
+	pass
