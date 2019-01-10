25379127bd7635d037de21796574b559989d5660
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-10 11:44:02 -0800

    Print a string Sinusoidally

---
+++ strings/print_a_string_sinusoidally.py
@@ -0,0 +1,18 @@
+'''
+Problem Statement: Print a string Sinusoidally
+
+[This is just a stupid problem, that has no relation to anything else. It's there primarily because we see it on and off. It's a string puzzle disguised as a programming problem]
+
+Also called "SnakeString". For example, the phrase "Google Worked" should be printed as follows (where ~ is the word separator):
+
+    o     ~       k
+  o  g  e  W  r   e
+G     l      o      d
+
+Note:
+https://www.geeksforgeeks.org/print-string-wave-pattern/
+
+'''
+
+def print_string_sinusoidally(s):
+	pass
