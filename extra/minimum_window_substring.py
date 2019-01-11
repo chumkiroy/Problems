98c4bc5773d63c44ad8f3a2a4446c2a7b1f0b542
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-11 00:29:02 -0800

    Minimum Window Substring

---
+++ extra/minimum_window_substring.py
@@ -0,0 +1,20 @@
+'''
+Problem Statement: Minimum window substring
+
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T.
+e.g.
+S = "AYZABOBECODXBANC"
+T = "ABC"
+
+Minimum window is "BANC",which contains all letters - A B and C.
+* If no such window exists, then return an empty string 
+* If there are multiple minimum windows of the same length, then return any one
+* Characters may be repeated
+
+Note:
+https://leetcode.com/problems/minimum-window-substring/description/
+
+'''
+
+def  MinWindow(strText, strCharacters):
+	pass
