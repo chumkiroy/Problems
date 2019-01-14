dbf92db27375a7aa3ac693c082b7e7acdad6602a
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-14 01:06:12 -0800

    Minimum sliding window
    - Given a string S and a string T, find the minimum window in S which will contain all the characters in T.

---
+++ minimum_sliding_window.py
@@ -0,0 +1,43 @@
+'''
+Problem Statement: Minimum sliding window
+
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T.
+
+
+
+e.g.
+
+S = "AYZABOBECODXBANC"
+
+T = "ABC"
+
+
+
+Minimum window is "BANC",which contains all letters - A B and C.
+
+
+
+* If no such window exists, then return an empty string 
+
+* If there are multiple minimum windows of the same length, then return any one
+
+* Characters may be repeated
+
+
+
+Note:
+
+
+
+This problem is under development according to IK standards. If you'd like to contribute, then please feel free to email soham@interviewkickstart.com
+
+
+
+Till we finish developing this problem, you can look at:
+
+https://leetcode.com/problems/minimum-window-substring/description/
+
+'''
+
+def  MinWindow(strText, strCharacters):
+	pass
