f2eb248e6e60f2853c52ac6558dc70e75b7beace
Chumki Roy <chumki.a.roy@gmail.com>
2019-01-23 00:30:42 -0800

    Minimum Window Substring
    - Given a string S and a string T, find the minimum window in S which will contain all the characters in T.

--- strings/minimum_window_substring.py
+++ strings/minimum_window_substring.py
@@ -16,5 +16,43 @@ Note:
 https://leetcode.com/problems/minimum-window-substring/description/
 
 '''
-def  MinWindow(strText, strCharacters):
-	pass
+from collections import Counter
+
+def  MinWindow(s, t):
+	if not t or not s:
+            return ""
+        
+        dict_t = Counter(t)
+        required = len(dict_t)
+        
+        l, r = 0, 0
+        unique_char_in_window = 0
+        window = {}
+        
+        ans = float('inf'), None, None
+        
+        while r < len(s):
+            char = s[r]
+            window[char] = window.get(char, 0) + 1
+            if char in dict_t and window[char] == dict_t[char]:
+                unique_char_in_window += 1
+                
+            while l <= r and unique_char_in_window == required:
+                char = s[l]
+                
+                if r-l+1 < ans[0]:
+                    ans = (r-l+1, l, r)
+                    
+                window[char] -=1
+                if char in dict_t and window[char] < dict_t[char]:
+                    unique_char_in_window -= 1
+                
+                l += 1
+            
+            r += 1
+            
+        return "" if ans[0] == float('inf') else s[ans[1]:ans[2]+1]
+
+S = "AYZABOBECODXBANC"
+T = "ABC"
+print MinWindow(S, T)
